9151ac8b9cfc724b8d0db10aeade54b746e542e9
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2025-09-15 06:39:00 -0400

    Updated hello world dag

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 
 def hello_world():
-    print("Hello, Raffi!")
+    print("Hello, World!")
 
 with DAG(
     dag_id='hello_world_dag',
